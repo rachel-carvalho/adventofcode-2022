b13a2713965f2f770f83428d8a9a73784d4bcb1f
Rachel Carvalho <rachel.carvalho@gmail.com>
2022-12-04 10:44:03 -0500

    [day 3] part 1 solution

--- day_03/responses.rb
+++ day_03/responses.rb
@@ -2,8 +2,8 @@ require_relative './solver'
 
 input = File.read(File.expand_path('./input.txt', __dir__))
 
-puts 'part 1:'
-# puts Solver.new(input).repeated_item_priority_sum
+puts 'part 1:' # 8349
+puts Solver.new(input).repeated_item_priority_sum
 
 puts 'part 2:'
 # puts Solver.new(input).repeated_item_priority_sum
--- day_03/solver.rb
+++ day_03/solver.rb
@@ -4,6 +4,29 @@ class Solver
   attr_reader :rucksacks
 
   def initialize(input)
-    @rucksacks = input.split("\n")
+    @rucksacks = input.split("\n").map { |line| Rucksack.new(line) }
+  end
+
+  def repeated_item_priority_sum
+    rucksacks.sum(&:repeated_item_priority)
+  end
+end
+
+class Rucksack
+  attr_reader :all_items, :first_compartment, :second_compartment
+
+  def initialize(all_items)
+    @all_items = all_items.split('')
+    @first_compartment, @second_compartment = @all_items.each_slice(all_items.length / 2).to_a
+  end
+
+  def repeated_item
+    @repeated_item ||= (first_compartment & second_compartment).first
+  end
+
+  PRIORITIES = ['0', *('a'..'z'), *('A'..'Z')].freeze
+
+  def repeated_item_priority
+    PRIORITIES.index(repeated_item)
   end
 end
