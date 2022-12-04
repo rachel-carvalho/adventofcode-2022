6ae7a6aa2c0b06026b6844776ed1691ea68dc1ba
Rachel Carvalho <rachel.carvalho@gmail.com>
2022-12-04 11:39:18 -0500

    [day 4] part 1 solution

--- day_04/solver.rb
+++ day_04/solver.rb
@@ -1,1 +1,30 @@
 # frozen_string_literal: true
+
+class Solver
+  def initialize(input)
+    @input = input
+  end
+
+  def pairs
+    @pairs ||= @input.split("\n").map { |line| Pair.new(line) }
+  end
+
+  def fully_overlapping_pair_count
+    pairs.count(&:fully_overlap?)
+  end
+end
+
+class Pair
+  attr_reader :first_section_range, :second_section_range
+
+  def initialize(input)
+    @first_section_range, @second_section_range = input.split(',').map do |elf|
+      lower_bound, upper_bound = elf.split('-')
+      lower_bound.to_i..upper_bound.to_i
+    end
+  end
+
+  def fully_overlap?
+    first_section_range.cover?(second_section_range) || second_section_range.cover?(first_section_range)
+  end
+end
--- day_04/solver_test.rb
+++ day_04/solver_test.rb
@@ -20,27 +20,27 @@ describe Solver do
       assert_equal 6, s.pairs.count
     end
 
-    # it 'expands each pair section range' do
-    #   s = Solver.new(@input)
-    #   pair = s.pairs.first
-    #   assert_equal 2..4, pair.first_section_range
-    #   assert_equal 6..8, pair.second_section_range
-    # end
-    #
-    # it 'detects full overlaps of sections' do
-    #   s = Solver.new(@input)
-    #   assert_equal false, s.pairs.first.fully_overlap?
-    #   assert_equal false, s.pairs.second.fully_overlap?
-    #   assert_equal false, s.pairs.third.fully_overlap?
-    #   assert_equal true, s.pairs.fourth.fully_overlap?
-    #   assert_equal true, s.pairs.fifth.fully_overlap?
-    #   assert_equal false, s.pairs[5].fully_overlap?
-    # end
-    #
-    # it 'counts pairs that overlap completely' do
-    #   s = Solver.new(@input)
-    #   assert_equal 2, s.fully_overlapping_pair_count
-    # end
+    it 'expands each pair section range' do
+      s = Solver.new(@input)
+      pair = s.pairs.first
+      assert_equal 2..4, pair.first_section_range
+      assert_equal 6..8, pair.second_section_range
+    end
+
+    it 'detects full overlaps of sections' do
+      s = Solver.new(@input)
+      assert_equal false, s.pairs.first.fully_overlap?
+      assert_equal false, s.pairs.second.fully_overlap?
+      assert_equal false, s.pairs.third.fully_overlap?
+      assert_equal true, s.pairs.fourth.fully_overlap?
+      assert_equal true, s.pairs.fifth.fully_overlap?
+      assert_equal false, s.pairs[5].fully_overlap?
+    end
+
+    it 'counts pairs that overlap completely' do
+      s = Solver.new(@input)
+      assert_equal 2, s.fully_overlapping_pair_count
+    end
   end
 
   describe 'part 2 - ?' do
