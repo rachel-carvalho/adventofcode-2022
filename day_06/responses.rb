42848cf17e51a0bebbdf3f9035ef9090b35f6f3b
Rachel Carvalho <rachel.carvalho@shopify.com>
2022-12-07 15:41:39 -0500

    [day 6] solve part 1

--- day_06/responses.rb
+++ day_06/responses.rb
@@ -2,7 +2,7 @@ require_relative './solver'
 
 input = File.read(File.expand_path('./input.txt', __dir__))
 
-puts 'part 1:' # ?
+puts 'part 1:' # 1080
 puts Solver.new(input).start_of_packet_index
 
 puts 'part 2:' # ?
--- day_06/solver.rb
+++ day_06/solver.rb
@@ -3,5 +3,26 @@ require 'active_support/all'
 
 class Solver
   def initialize(input)
+    @input = input
+  end
+
+  MARKER_SIZE = 4
+
+  def start_of_packet_marker
+    return @start_of_packet_marker unless @start_of_packet_marker.nil?
+
+    characters = @input.chars
+    characters.each.with_index(1).each do |letter, position|
+      next if position < MARKER_SIZE
+
+      from = position - MARKER_SIZE
+      to = from + MARKER_SIZE
+      last_4_chars = characters[from...to].uniq
+      return @start_of_packet_marker = last_4_chars.join if last_4_chars.count == MARKER_SIZE
+    end
+  end
+
+  def start_of_packet_index
+    @input.index(start_of_packet_marker) + MARKER_SIZE
   end
 end
