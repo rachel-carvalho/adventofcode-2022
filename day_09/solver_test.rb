6c289acebec2be130adb04c8945906905e36ab83
Rachel Carvalho <rachel.carvalho@gmail.com>
2022-12-09 10:49:18 -0500

    [day 9] add part 1

---
+++ day_09/solver_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative './solver'
+
+describe Solver do
+  before do
+    @input = '''
+'''.strip
+  end
+
+  describe 'part 1 - ' do
+
+  end
+
+  describe 'part 2 - ?' do
+  end
+end
